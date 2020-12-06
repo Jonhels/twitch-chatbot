06a32c9e1e5b0dbcf346ad59d81536d52a4dbf22
Jonhels <jonhels8888@gmail.com>
2020-12-06 20:43:20 +0100

    done

--- bot.js
+++ bot.js
@@ -33,6 +33,60 @@ function onMessageHandler (target, context, msg, self) {
         client.say(target, `you rolled a ${num}`);
         console.log(`* Executed ${commandName} command`);
     }
+
+    if (commandName === '!clap') { 
+        client.say(target, `AYAYA Clap AYAYA Clap AYAYA Clap AYAYA Clap`);
+    }
+
+    if (commandName === '!dance') {
+        client.say(target, `PepePls catJAM PepePls catJAM `)
+    }
+
+    if (commandName === '!jam') {
+        client.say(target, `catJAM catJAM catJAM catJAM`)
+    }
+
+    if (commandName === '!sadjam') {
+        client.say(target, `PepePls PepePls PepePls PepePls`)
+    }
+
+    if (commandName === '!feed') {
+        client.say(target, `FeelsAmazingMan FeelsAmazingMan FeelsAmazingMan FeelsAmazingMan`)
+    }
+
+    if (commandName === '!weird') {
+        client.say(target, `weirdChamp weirdChamp weirdChamp weirdChamp`)
+    }
+
+    if (commandName === '!melt') {
+        client.say(target, `pepeMeltdown pepeMeltdown pepeMeltdown pepeMeltdown`)
+    }
+
+    if (commandName === '!hyper') {
+        client.say(target, `gachiHYPER gachiHYPER gachiHYPER gachiHYPER `)
+    }
+
+    if (commandName === '!pog') {
+        client.say(target, `PagChomp PagChomp PagChomp PagChomp `)
+    }
+    
+    if (commandName === '!commands') {
+        client.say(target, `
+        These are all the emote commands:
+        !clap = 4x AYAYA Clap ,
+        !dance = 2x PepePls catJAM ,
+        !jam =  4x catJAM ,
+        !sadjam = 4x PepePls ,
+        !feed = 4x FeelsAmazingMan ,
+        !weird = 4x weirdChamp ,
+        !melt = 4x pepeMeltdown , 
+        !hyper = 4x gachiHYPER ,
+        !pog = 4x PagChomp
+        `)
+    }
+    
+    
+
 }
 
 // Function called when the "dice" command is issued
